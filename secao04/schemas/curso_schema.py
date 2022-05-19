0bce230fd6b54a245a2efa04d02a5cdadb3f508e
CleberNandi <cleber.nandi@gmail.com>
2022-05-19 10:59:28 -0400

    Adicionado schemas

---
+++ secao04/schemas/curso_schema.py
@@ -0,0 +1,13 @@
+from tokenize import Intnumber
+from typing import Optional
+
+from pydantic import BaseModel as SCBaseModel
+
+class CursoSchema(SCBaseModel):
+    id: Optional[int]
+    titulo: str
+    aulas: int
+    horas: int
+    
+    class Config:
+        orm_mode = True
